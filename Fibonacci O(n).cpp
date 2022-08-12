96998abaaf5862f4e449935e283925d6970b131c
Anshuman Prakash <anshu.pra00@gmail.com>
2022-08-12 09:47:35 +0530

    first commit

---
+++ Fibonacci O(n).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+    int a=0, b=1,c;
+    if(n==0){
+        return a;
+    }
+    for(int i=1;i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return b;
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<fibonacci(i)<<" ";
+    }
+    return 0;
+}
